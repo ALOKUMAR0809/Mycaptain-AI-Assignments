9d1ec387750ad33039030c06a0d9f25fdae244f9
ALOKUMAR0809 <115224029+ALOKUMAR0809@users.noreply.github.com>
2023-04-16 19:09:51 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,7 @@
+#hey, i used hashyag to get this line as comment
+#the assignmnet is to print hello world!
+#the code for that is in the next line
+print("Hello World!")
+#this was the code
+#this will give a output as Hello World!
+#thank you!!
